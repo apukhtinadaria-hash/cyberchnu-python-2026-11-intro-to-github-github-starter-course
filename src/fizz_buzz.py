9dcf5f303581fd28db256d3cb7c4023efc3369e5
LokalerUser <apukhtina.daria@chnu.edu.ua>
2026-05-11 21:58:00 +0300

    9

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,15 @@
 def fizz_buzz(param):
   #Type your code here
-  return
+  if param % 3 == 0 and param % 5 == 0:
+    return "FizzBuzz"
+  elif param % 3 == 0:
+    return "Fizz"
+  elif param % 5 == 0:
+    return "Buzz"
+  else:
+    return str(param)
+
+print(fizz_buzz(3))
+print(fizz_buzz(5))
+print(fizz_buzz(15))
+print(fizz_buzz(4))
