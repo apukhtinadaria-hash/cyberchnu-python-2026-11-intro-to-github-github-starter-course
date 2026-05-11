43de1089fabc86c3b40df82bca26fb5c92192c6a
LokalerUser <apukhtina.daria@chnu.edu.ua>
2026-05-11 21:51:44 +0300

    10

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,10 @@
 def mean(number):
   # Type your code
-  return 
+  digits = str(number)
+  total = 0
+  for d in digits:
+    total += int(d)
+  return total/len(digits)
+
+print(mean(42))
+print(mean(12345))
