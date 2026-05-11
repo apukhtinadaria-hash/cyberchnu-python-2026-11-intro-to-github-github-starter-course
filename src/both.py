8c76d95b3180bd93ead3ff062132c7a8d527ab19
LokalerUser <apukhtina.daria@chnu.edu.ua>
2026-05-11 22:23:36 +0300

    5

--- src/both.py
+++ src/both.py
@@ -1,3 +1,14 @@
 def both(number1, number2):
   # Type your code
-  return
+  if number1 > 0 and number2 > 0:
+    return True
+  elif number1 < 0 and number2 < 0:
+    return True
+  elif number1 == 0 and number2 == 0:
+    return True
+  else:
+    return False
+
+
+print(both(6, 2))
+print(both(-2, 5))
