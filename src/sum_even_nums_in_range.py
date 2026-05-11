b0494166f728d998cb35d24461a5df7d87ef965a
LokalerUser <apukhtina.daria@chnu.edu.ua>
2026-05-11 22:22:02 +0300

    9

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,9 @@
 def sum_even_nums_in_range(start, stop):
   # Type your code
-  return
+  total = 0
+  for num in range(start, stop + 1):
+    if num % 2 == 0:
+      total += num
+  return total
+
+print(sum_even_nums_in_range(0, 10))
